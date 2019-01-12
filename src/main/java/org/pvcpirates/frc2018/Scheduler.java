4c65497fd0b46d13a25e20a92cd8f93f603eaea4
Matt Mayer <mmayer@mitre.org>
2019-01-12 09:55:19 -0500

    Remove teleop hardware initializations

--- src/main/java/org/pvcpirates/frc2018/Scheduler.java
+++ src/main/java/org/pvcpirates/frc2018/Scheduler.java
@@ -86,12 +86,6 @@ public class Scheduler extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		robot.state.exec();
-		Hardware.getInstance().armExtendMotor.configPeakCurrentLimit(40000, 10);
-		Hardware.getInstance().armExtendMotor.enableCurrentLimit(false);
-		Hardware.getInstance().armExtendMotorFollower.configPeakCurrentLimit(40000, 10);
-		Hardware.getInstance().armExtendMotorFollower.enableCurrentLimit(false);
-		System.out.println("Curr: "+Hardware.getInstance().armExtendMotor.getOutputCurrent()); 
-		
 	}
 
 	@Override

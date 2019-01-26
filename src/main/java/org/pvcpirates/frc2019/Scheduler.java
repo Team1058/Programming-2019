05947d7456f6ca5bcf575dbc6ca6e3aa2a6bb31e
DriverStation <FryDThundor@gmail.com>
2019-01-25 19:44:27 -0500

    Added a tab for PID and editable PID values

--- src/main/java/org/pvcpirates/frc2019/Scheduler.java
+++ src/main/java/org/pvcpirates/frc2019/Scheduler.java
@@ -3,6 +3,7 @@ package org.pvcpirates.frc2019;
 import org.pvcpirates.frc2019.robot.Robot;
 import org.pvcpirates.frc2019.state.AutoState;
 import org.pvcpirates.frc2019.state.TeleopState;
+import org.pvcpirates.frc2019.util.ShuffleBoardManager;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
@@ -12,6 +13,7 @@ public class Scheduler extends IterativeRobot {
 
 	@Override
 	public void robotInit() {
+		ShuffleBoardManager.initializeShuffleBoard();
 	  robot.setState(new TeleopState());
 	  robot.state.init();
 	}
--- src/main/java/org/pvcpirates/frc2019/robot/subsystems/Drivetrain.java
+++ src/main/java/org/pvcpirates/frc2019/robot/subsystems/Drivetrain.java
@@ -5,11 +5,29 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import org.pvcpirates.frc2019.robot.Hardware;
 import org.pvcpirates.frc2019.util.RobotMap;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import org.pvcpirates.frc2019.util.ShuffleBoardManager;
 
 public class Drivetrain extends BaseSubsystem {
+    private ShuffleBoardManager m_teamSB_SuffleBoardManeger = new ShuffleBoardManager();
+    // This Is For The PID Numbers;
+    public static double LEFT_PEAK_OUTPUT = .95;
+    public static double RIGHT_PEAK_OUTPUT = .95;
+    public static double LEFTDRIVE_F = .104;
+    public static double LEFTDRIVE_P = 0.01;
+    public static double LEFTDRIVE_I = 0;
+    public static double LEFTDRIVE_D = 0;
+    // RIGHT DRIVETRAIN;
+    public static double RIGHTDRIVE_F = .115;
+    public static double RIGHTDRIVE_P = 0.1;
+    public static double RIGHTDRIVE_I = 0;
+    public static double RIGHTDRIVE_D = 0;
+// TALON SRX
     public final TalonSRX leftDrive1 = new TalonSRX(RobotMap.CANTalonIds.LEFT_DRIVE_1);
     public final TalonSRX rightDrive1 = new TalonSRX(RobotMap.CANTalonIds.RIGHT_DRIVE_1);
     public final TalonSRX leftDrive2 = new TalonSRX(RobotMap.CANTalonIds.LEFT_DRIVE_2);
@@ -46,26 +64,44 @@ public class Drivetrain extends BaseSubsystem {
         leftDrive2.follow(leftDrive1);
         rightDrive2.follow(rightDrive1);
     }
-
-    private void initializeSetDrivePIDValues(){
-        leftDrive1.configClosedLoopPeakOutput(0, .95);
-        rightDrive1.configClosedLoopPeakOutput(0, .95);
-
-        leftDrive1.config_kF(0, .104);
-        leftDrive1.config_kP(0, .01);
-        leftDrive1.config_kI(0, 0);
-        leftDrive1.config_kD(0, 0);
     
-        rightDrive1.config_kF(0, .115);
-        rightDrive1.config_kP(0, .1);
-        rightDrive1.config_kI(0, 0);
-        rightDrive1.config_kD(0, 0);
+    public void initializeDrivetrainPIDGetShuffle(){
+        // LeftDrive PID Values;
+        ShuffleBoardManager.pidTab.add("leftdrive_F", LEFTDRIVE_F);
+        /*ShuffleBoardManager.pidTab.add("leftdrive_P",LEFTDRIVE_P);
+        ShuffleBoardManager.pidTab.add("leftdrive_I", LEFTDRIVE_I);
+        ShuffleBoardManager.pidTab.add("leftdrive_D", LEFTDRIVE_D);
+        // RightDrive PID Values
+        ShuffleBoardManager.pidTab.add("rightdrive_F", RIGHTDRIVE_F);
+        ShuffleBoardManager.pidTab.add("rightdrive_P", RIGHTDRIVE_P);
+        ShuffleBoardManager.pidTab.add("rightdrive_I",RIGHTDRIVE_I);
+        ShuffleBoardManager.pidTab.add("rightdrive_D", RIGHTDRIVE_D);
+        ShuffleBoardManager.pidTab.add("leftdrive_loopoutput", LEFT_PEAK_OUTPUT);
+        ShuffleBoardManager.pidTab.add("rightdrive_loopoutput", RIGHT_PEAK_OUTPUT);    
+        */
+    }
+
+    public void initializeSetDrivePIDValues(){
+        initializeDrivetrainPIDGetShuffle();
+        leftDrive1.configClosedLoopPeakOutput(0, LEFT_PEAK_OUTPUT);
+        rightDrive1.configClosedLoopPeakOutput(0, RIGHT_PEAK_OUTPUT);
+
+        leftDrive1.config_kF(0, SmartDashboard.getNumber("leftdrive_F", LEFTDRIVE_F));
+        leftDrive1.config_kP(0, SmartDashboard.getNumber("leftdrive_P",LEFTDRIVE_P));
+        leftDrive1.config_kI(0, SmartDashboard.getNumber("leftdrive_I", LEFTDRIVE_I));
+        leftDrive1.config_kD(0, SmartDashboard.getNumber("leftdrive_D", LEFTDRIVE_D));
+        rightDrive1.config_kF(0, SmartDashboard.getNumber("rightdrive_F", RIGHTDRIVE_F));
+        rightDrive1.config_kP(0, SmartDashboard.getNumber("rightdrive_P", RIGHTDRIVE_P));
+        rightDrive1.config_kI(0, SmartDashboard.getNumber("rightdrive_I",RIGHTDRIVE_I));
+        rightDrive1.config_kD(0, SmartDashboard.getNumber("rightdrive_D", RIGHTDRIVE_D));
+        
     }
 
     public void stopAll() {
         // Shut everything off
         leftDrive1.set(ControlMode.PercentOutput, 0);
         rightDrive1.set(ControlMode.PercentOutput, 0);
+        
     }
 
     public void setDrive(ControlMode controlMode, double left, double right) {

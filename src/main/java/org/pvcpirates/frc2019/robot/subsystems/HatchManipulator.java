fde8c2f05d941720d80a37dd8528b0ec1d313774
DriverStation <FryDThundor@gmail.com>
2019-02-02 13:51:29 -0500

    Added HatchManipulationCommand waiting for comments

--- src/main/java/org/pvcpirates/frc2019/robot/subsystems/HatchManipulator.java
+++ src/main/java/org/pvcpirates/frc2019/robot/subsystems/HatchManipulator.java
@@ -5,28 +5,41 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class HatchManipulator extends BaseSubsystem {
 
-    //public final Compressor compressor = new Compressor();
-    //public final DoubleSolenoid clawSolenoid = new DoubleSolenoid();
-    //public final DoubelSolenoid sliderSolenoid = new DoubleSolenoid();
+    int doubleClawSolenoidForwardPCM = 0;
+    int doubleClawSolenoidReversePCM = 0;
+    int doubleSliderSolenoidForwardPCM = 0;
+    int doubleSliderSolenoidReversePCM = 0;
+
+    public final Compressor compressor = new Compressor();
+    public final DoubleSolenoid clawSolenoid = new DoubleSolenoid(doubleClawSolenoidForwardPCM,doubleClawSolenoidReversePCM);
+    public final DoubleSolenoid sliderSolenoid = new DoubleSolenoid(doubleSliderSolenoidForwardPCM,doubleSliderSolenoidReversePCM);
     
     public void initialize(){
 
     }
 
-    public void hatchClawIn(){
-
+    public void hatchClawRetract(){
+        if (clawSolenoid.get() != DoubleSolenoid.Value.kReverse){
+            sliderSolenoid.set(DoubleSolenoid.Value.kReverse);
+        }
     }
 
-    public void hatchClawOut() {
-
+    public void hatchClawExpand() {
+        if (clawSolenoid.get() != DoubleSolenoid.Value.kForward){
+            sliderSolenoid.set(DoubleSolenoid.Value.kForward);
+        }
     }
 
     public void hatchSliderIn(){
-        
+        if (sliderSolenoid.get() != DoubleSolenoid.Value.kReverse){
+            sliderSolenoid.set(DoubleSolenoid.Value.kReverse);
+        }
     }
 
     public void hatchSliderOut(){
-
+        if (sliderSolenoid.get() != DoubleSolenoid.Value.kForward){
+            sliderSolenoid.set(DoubleSolenoid.Value.kForward);
+        }
     }
 
 }

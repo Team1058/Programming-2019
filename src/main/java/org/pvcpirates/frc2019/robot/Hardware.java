3f23caea97045616e6fb7752ef94130a9fb33fd0
Liam McIntyre <mcintyrelr20@gmail.com>
2019-02-09 15:04:52 -0500

    add battery voltage check

--- src/main/java/org/pvcpirates/frc2019/robot/Hardware.java
+++ src/main/java/org/pvcpirates/frc2019/robot/Hardware.java
@@ -3,10 +3,12 @@ package org.pvcpirates.frc2019.robot;
 import org.pvcpirates.frc2019.robot.subsystems.*;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import org.pvcpirates.frc2019.util.ShuffleBoardManager;
 public class Hardware {
-
+    ShuffleboardTab tab = Shuffleboard.getTab("Battery_Voltage");
     public static ShuffleBoardManager sbm = new ShuffleBoardManager();
     private static Hardware ourInstance;
     private PowerDistributionPanel pdp = new PowerDistributionPanel(0);
@@ -41,15 +43,15 @@ public class Hardware {
     public void reportbatteryvoltage(){
         double voltage = pdp.getVoltage();
         if (voltage >= 12.0){
-            sbm.pidTab.add("The Battery is ready for competition.", voltage);
+            tab.add("The Battery is ready for competition.", voltage);
         }
             
         if (voltage >= 11.0 && voltage < 12.0){
-            sbm.pidTab.add("Warning! The batter is low but useable.", voltage);
+            tab.add("Warning! The battery is low but useable.", voltage);
 
         }
         if (voltage < 11.0){
-            sbm.pidTab.add("DO NOT USE THIS BATTERY! ", voltage);
+            tab.add("DO NOT USE THIS BATTERY! ", voltage);
         }
 
     

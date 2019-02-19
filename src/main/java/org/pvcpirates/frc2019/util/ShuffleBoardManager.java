6842a603dbad093653696237f711dd596355730b
DriverStation <FryDThundor@gmail.com>
2019-02-19 15:28:54 -0500

    fixed an syntax error

--- src/main/java/org/pvcpirates/frc2019/util/ShuffleBoardManager.java
+++ src/main/java/org/pvcpirates/frc2019/util/ShuffleBoardManager.java
@@ -89,9 +89,9 @@ public class ShuffleBoardManager {
     }
 
     private static void initializeCompetitionTab(){
-        visionTargetBool = competitionTab.add("visionTarget",false).getEntry();
-        hatchClawGrabEntry = competitionTab.add("hatchClawGrab",false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
-        hatchClawPrepGEntry = competitionTab.add("hatchClawPrepGrab",false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
+        visionTargetBool = competitionTab.addPersistent("visionTarget",false).getEntry();
+        hatchClawGrabEntry = competitionTab.addPersistent("hatchClawGrab",false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
+        hatchClawPrepGEntry = competitionTab.addPersistent("hatchClawPrepGrab",false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
         initializeFlipperComboBox();
     }
 
@@ -107,59 +107,59 @@ public class ShuffleBoardManager {
     }
 
     private static void initializeMaintainanceTab(){
-        leftDriveSpeedEntry = maintainanceTab.add("leftDriveSpeed", 0).getEntry();
-        rightDriveSpeedEntry = maintainanceTab.add("rightDriveSpeed",0).getEntry();
-        leftJoyYaxisEntry = maintainanceTab.add("leftJoyYAxis", 0).getEntry();
-        rightJoyYaxisEntry = maintainanceTab.add("rightJoyXAxis",0).getEntry();
-        visionDiagEntry = maintainanceTab.add("visionDiag",0).getEntry();
+        leftDriveSpeedEntry = maintainanceTab.addPersistent("leftDriveSpeed", 0).getEntry();
+        rightDriveSpeedEntry = maintainanceTab.addPersistent("rightDriveSpeed",0).getEntry();
+        leftJoyYaxisEntry = maintainanceTab.addPersistent("leftJoyYAxis", 0).getEntry();
+        rightJoyYaxisEntry = maintainanceTab.addPersistent("rightJoyXAxis",0).getEntry();
+        visionDiagEntry = maintainanceTab.addPersistent("visionDiag",0).getEntry();
 
-        flipperDefaultPositionEntry = maintainanceTab.add("flipperDefaultPosition", Flipper.defaultPosConstant).getEntry();
-        flipperlvl0To2BackEntry = maintainanceTab.add("flipperlvl0To2Back", Flipper.lvl0to2BackConstant).getEntry();
-        flipperlvl0To2FrontEntry = maintainanceTab.add("flipperlvl0To2Front", Flipper.lvl0to2FrontConstant).getEntry();
-        flipperlvl2To3BackEntry = maintainanceTab.add("flipperlvl2To3Back", Flipper.lvl2to3BackConstant).getEntry();
-        flipperlvl2To3FrontEntry = maintainanceTab.add("flipperlvl2To3Front", Flipper.lvl2to3FrontConstant).getEntry();
-        elevatorIntakeSetpointEntry = maintainanceTab.add("elevatorIntakeSetpoint",Elevator.intakeSetpoint).getEntry();
-        elevatorDefaultSetpointEntry = maintainanceTab.add("elevatorDefaultSetpoint",Elevator.defaultSetpoint).getEntry();
-        elevatorHatchLowSetpointEntry = maintainanceTab.add("elevatorHatchLowSetpoint",Elevator.hatchLowSetpoint).getEntry();
-        elevatorHatchMidSetpointEntry = maintainanceTab.add("elevatorHatchMidSetpoint",Elevator.hatchMidSetpoint).getEntry();
-        elevatorHatchHighSetpointEntry = maintainanceTab.add("elevatorHatchHighSetpoint",Elevator.hatchHighSetpoint).getEntry();
-        elevatorCargoLowSetpointEntry = maintainanceTab.add("elevatorCargoLow",Elevator.cargoLowSetpoint).getEntry();
-        elevatorCargoMidSetpointEntry = maintainanceTab.add("elevatorCargoMid",Elevator.cargoMidSetpoint).getEntry();
+        flipperDefaultPositionEntry = maintainanceTab.addPersistent("flipperDefaultPosition", Flipper.defaultPosConstant).getEntry();
+        flipperlvl0To2BackEntry = maintainanceTab.addPersistent("flipperlvl0To2Back", Flipper.lvl0to2BackConstant).getEntry();
+        flipperlvl0To2FrontEntry = maintainanceTab.addPersistent("flipperlvl0To2Front", Flipper.lvl0to2FrontConstant).getEntry();
+        flipperlvl2To3BackEntry = maintainanceTab.addPersistent("flipperlvl2To3Back", Flipper.lvl2to3BackConstant).getEntry();
+        flipperlvl2To3FrontEntry = maintainanceTab.addPersistent("flipperlvl2To3Front", Flipper.lvl2to3FrontConstant).getEntry();
+        elevatorIntakeSetpointEntry = maintainanceTab.addPersistent("elevatorIntakeSetpoint",Elevator.intakeSetpoint).getEntry();
+        elevatorDefaultSetpointEntry = maintainanceTab.addPersistent("elevatorDefaultSetpoint",Elevator.defaultSetpoint).getEntry();
+        elevatorHatchLowSetpointEntry = maintainanceTab.addPersistent("elevatorHatchLowSetpoint",Elevator.hatchLowSetpoint).getEntry();
+        elevatorHatchMidSetpointEntry = maintainanceTab.addPersistent("elevatorHatchMidSetpoint",Elevator.hatchMidSetpoint).getEntry();
+        elevatorHatchHighSetpointEntry = maintainanceTab.addPersistent("elevatorHatchHighSetpoint",Elevator.hatchHighSetpoint).getEntry();
+        elevatorCargoLowSetpointEntry = maintainanceTab.addPersistent("elevatorCargoLow",Elevator.cargoLowSetpoint).getEntry();
+        elevatorCargoMidSetpointEntry = maintainanceTab.addPersistent("elevatorCargoMid",Elevator.cargoMidSetpoint).getEntry();
 
-        fourBarLow = maintainanceTab.add("fourBarLow",Elevator.fourBarLowSetpoint).getEntry();
-        fourBarMid = maintainanceTab.add("fourBarMid",Elevator.fourBarMidSetpoint).getEntry();
-        fourBarHigh = maintainanceTab.add("fourBarHigh",Elevator.fourBarHighSetpoint).getEntry();
-        miniWheelControl = maintainanceTab.add("miniWheelPercentOutput",0).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+        fourBarLow = maintainanceTab.addPersistent("fourBarLow",Elevator.fourBarLowSetpoint).getEntry();
+        fourBarMid = maintainanceTab.addPersistent("fourBarMid",Elevator.fourBarMidSetpoint).getEntry();
+        fourBarHigh = maintainanceTab.addPersistent("fourBarHigh",Elevator.fourBarHighSetpoint).getEntry();
+        miniWheelControl = maintainanceTab.addPersistent("miniWheelPercentOutput",0).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
         flipperPercentOutputEntry = maintainanceTab.addPersistent("flipperPercentOutput",0).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
         elevatorPercentOutputEntry = maintainanceTab.addPersistent("elevatorPercentOutput", 0).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
     }
 
     private static void initializePIDTab(){
 
-        fFlipperEntry = pidTab.add("flipper_F",Flipper.FLIPPER_F).withWidget(BuiltInWidgets.kTextView).getEntry();
-        pFlipperEntry = pidTab.add("flipper_P",Flipper.FLIPPER_P).withWidget(BuiltInWidgets.kTextView).getEntry();
-        iFlipperEntry = pidTab.add("flipper_I",Flipper.FLIPPER_I).withWidget(BuiltInWidgets.kTextView).getEntry();
-        dFlipperEntry = pidTab.add("flipper_D",Flipper.FLIPPER_D).withWidget(BuiltInWidgets.kTextView).getEntry();
-        pStowedFlipperEntry = pidTab.add("flipper_P_stowed",Flipper.FLIPPER_P_STOWED).withWidget(BuiltInWidgets.kTextView).getEntry();
+        fFlipperEntry = pidTab.addPersistent("flipper_F",Flipper.FLIPPER_F).withWidget(BuiltInWidgets.kTextView).getEntry();
+        pFlipperEntry = pidTab.addPersistent("flipper_P",Flipper.FLIPPER_P).withWidget(BuiltInWidgets.kTextView).getEntry();
+        iFlipperEntry = pidTab.addPersistent("flipper_I",Flipper.FLIPPER_I).withWidget(BuiltInWidgets.kTextView).getEntry();
+        dFlipperEntry = pidTab.addPersistent("flipper_D",Flipper.FLIPPER_D).withWidget(BuiltInWidgets.kTextView).getEntry();
+        pStowedFlipperEntry = pidTab.addPersistent("flipper_P_stowed",Flipper.FLIPPER_P_STOWED).withWidget(BuiltInWidgets.kTextView).getEntry();
 
-        fDriveEntry = pidTab.add("drive_F", Drivetrain.DRIVE_F).getEntry();
-        pDriveEntry = pidTab.add("drive_P", Drivetrain.DRIVE_P).getEntry();
-        iDriveEntry = pidTab.add("drive_I", Drivetrain.DRIVE_I).getEntry();
-        dDriveEntry = pidTab.add("drive_D", Drivetrain.DRIVE_D).getEntry();
-        loopDriveEntry = pidTab.add("drive_LoopOutput", Drivetrain.DRIVE_PEAK_OUTPUT).getEntry();
+        fDriveEntry = pidTab.addPersistent("drive_F", Drivetrain.DRIVE_F).getEntry();
+        pDriveEntry = pidTab.addPersistent("drive_P", Drivetrain.DRIVE_P).getEntry();
+        iDriveEntry = pidTab.addPersistent("drive_I", Drivetrain.DRIVE_I).getEntry();
+        dDriveEntry = pidTab.addPersistent("drive_D", Drivetrain.DRIVE_D).getEntry();
+        loopDriveEntry = pidTab.addPersistent("drive_LoopOutput", Drivetrain.DRIVE_PEAK_OUTPUT).getEntry();
         
-        fElevatorEntry = pidTab.add("elevator_F",Elevator.ELEVATOR_F).getEntry();
-        pElevatorEntry = pidTab.add("elevator_P",Elevator.ELEVATOR_P).getEntry();
-        iElevatorEntry = pidTab.add("elevator_I",Elevator.ELEVATOR_I).getEntry();
-        dElevatorEntry = pidTab.add("elevator_D",Elevator.ELEVATOR_D).getEntry();
-        loopElevatorEntry = pidTab.add("elevator_closed_loop_ramp",Elevator.ELEVATOR_LOOP_RAMP_RATE).getEntry();
-        maxAccelElevatorEntry = pidTab.add("elevator_max_accel",Elevator.ELEVATOR_MAX_ACCEL).getEntry();
-        minVelocElevatorEntry = pidTab.add("elevator_min_veloc",Elevator.ELEVATOR_MIN_VELOC).getEntry();
-        maxVelocElevatorEntry = pidTab.add("elevator_max_veloc",Elevator.ELEVATOR_MAX_VELOC).getEntry();
+        fElevatorEntry = pidTab.addPersistent("elevator_F",Elevator.ELEVATOR_F).getEntry();
+        pElevatorEntry = pidTab.addPersistent("elevator_P",Elevator.ELEVATOR_P).getEntry();
+        iElevatorEntry = pidTab.addPersistent("elevator_I",Elevator.ELEVATOR_I).getEntry();
+        dElevatorEntry = pidTab.addPersistent("elevator_D",Elevator.ELEVATOR_D).getEntry();
+        loopElevatorEntry = pidTab.addPersistent("elevator_closed_loop_ramp",Elevator.ELEVATOR_LOOP_RAMP_RATE).getEntry();
+        maxAccelElevatorEntry = pidTab.addPersistent("elevator_max_accel",Elevator.ELEVATOR_MAX_ACCEL).getEntry();
+        minVelocElevatorEntry = pidTab.addPersistent("elevator_min_veloc",Elevator.ELEVATOR_MIN_VELOC).getEntry();
+        maxVelocElevatorEntry = pidTab.addPersistent("elevator_max_veloc",Elevator.ELEVATOR_MAX_VELOC).getEntry();
 
-        fFourBarEntry = pidTab.add("fFourBarEntry",Elevator.FOUR_BAR_F).getEntry();
-        pFourBarEntry = pidTab.add("pFourBarEntry",Elevator.FOUR_BAR_P).getEntry();
-        iFourBarEntry = pidTab.add("iFourBarEntry",Elevator.FOUR_BAR_I).getEntry();
-        dFourBarEntry = pidTab.add("dFourBarEntry",Elevator.FOUR_BAR_D).getEntry();
+        fFourBarEntry = pidTab.addPersistent("fFourBarEntry",Elevator.FOUR_BAR_F).getEntry();
+        pFourBarEntry = pidTab.addPersistent("pFourBarEntry",Elevator.FOUR_BAR_P).getEntry();
+        iFourBarEntry = pidTab.addPersistent("iFourBarEntry",Elevator.FOUR_BAR_I).getEntry();
+        dFourBarEntry = pidTab.addPersistent("dFourBarEntry",Elevator.FOUR_BAR_D).getEntry();
     }
 }

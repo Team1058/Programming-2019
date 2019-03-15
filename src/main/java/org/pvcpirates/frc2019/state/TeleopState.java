fed4416eff71282b6c7097cbf34836f468684a57
DriverStation <FryDThundor@gmail.com>
2019-03-15 19:33:46 -0400

    Commented out Operator Gamepad

--- src/main/java/org/pvcpirates/frc2019/state/TeleopState.java
+++ src/main/java/org/pvcpirates/frc2019/state/TeleopState.java
@@ -12,7 +12,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class TeleopState extends State {
     private DriverGamepad driverGamepad;
-    private OperatorGamepad operatorGamepad;
+    //private OperatorGamepad operatorGamepad;
     private OperatorButtonPad operatorButtonPad;
     private Hardware hardware;
 
@@ -20,15 +20,15 @@ public class TeleopState extends State {
     public void init() {
         hardware = Hardware.getInstance();
         driverGamepad = new DriverGamepad(0);
-        operatorGamepad = new OperatorGamepad(1);
-        operatorButtonPad = new OperatorButtonPad(2);
+        //operatorGamepad = new OperatorGamepad(1);
+        operatorButtonPad = new OperatorButtonPad(1);
     }
 
     @Override
     public void exec() {
         // Code here will all get called periodically (every ms) in Auto
         driverGamepad.executeCommands();
-        operatorGamepad.executeCommands();
+       // operatorGamepad.executeCommands();
         operatorButtonPad.executeCommands();
     }
 
